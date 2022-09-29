e93800bc8d2876219fcf7e77115c3c65e2b2930c
Amanda Erhart <erhart.a@c02tn69chtd6.localdomain>
2022-09-29 11:12:44 -0400

    more syntax

--- dataschema.sql
+++ dataschema.sql
@@ -6,7 +6,7 @@ CREATE TABLE quotes (
 
 INSERT INTO quotes (uuidkey, quote, author)
 VALUES ('4589f6c5-4129-4919-b3db-de45edee244b', 'Reflection is never clear.', 'Joe Burrow'),
-('adfec432-e3ca-4a18-9ce3-0db41ae67784', 'Don''t just check errors handle them gracefully.' 'Oprah'),
+('adfec432-e3ca-4a18-9ce3-0db41ae67784', 'Don''t just check errors handle them gracefully.', 'Oprah'),
 ('3736cfb7-1569-4cea-b9d7-cc438afa2ab8', 'A little copying is better than a little dependency.', 'Vienna Erhart'),
 ('2c9d9b91-e829-4495-8e7c-4991ddc8c5d6', 'The bigger the interface, the weaker the abstraction.', 'Josh Rose'),
 ('cca8abd8-37f8-4ed9-8853-2cfd734d6965',  'Don''t panic.', 'Queen of England');
